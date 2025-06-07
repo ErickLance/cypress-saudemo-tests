d273fdd673d8a62e2b139d16133216fef7cc12fb
Erick Lance <erickprofissional02@gmail.com>
2025-06-07 17:50:01 -0300

    feat: implementação da tela de login

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -11,3 +11,11 @@ Cypress.Commands.add('guiLogin', (
   // Assert
   cy.url().should('include', '/inventory.html')
 })
+
+Cypress.Commands.add('sessionLogin', (
+  username = Cypress.env('USER_NAME'),
+  password = Cypress.env('USER_PASSWORD')
+) => {
+  const login =() => cy.guiLogin(username, password)
+  cy.session(username, login)
+})
